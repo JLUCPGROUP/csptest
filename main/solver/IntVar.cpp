ce3eff35b8df7231d0a4fbcc2cfce081c05c91ba
李哲 <leezear@live.cn>
2017-12-09 15:58:56 +0800

    1234

---
+++ main/solver/IntVar.cpp
@@ -0,0 +1,20 @@
+﻿#include "IntVar.h"
+namespace cudacp {
+IntVar::IntVar(const int id, const string name, const vector<int> values) :
+	id_(id), cur_size_(values.size()), init_size_(values.size()), name_(name), dom_(values) {
+
+	for (int i = 0; i < cur_size_; ++i)
+		map_[dom_[i]] = i;
+
+}
+
+
+bool IntVar::constains(const int value) {
+	return map_[value] < cur_size_;
+}
+
+int IntVar::size() const {
+	return cur_size_;
+}
+
+}
---
+++ main/solver/IntVar.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+namespace cudacp {
+class IntVar {
+public:
+
+	IntVar() {};
+	IntVar(const int id, const string name, const vector<int> values);
+	bool constains(const int value);
+	int size() const ;
+protected:
+	int id_;
+	int cur_size_;
+	int init_size_;
+	string name_;
+	vector<int> dom_;
+	map<int, int> map_;
+};
+}
