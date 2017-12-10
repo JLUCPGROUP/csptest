e85d7f19e9e25c29ba68ab9e0861d9b856468228
李哲 <leezear@live.cn>
2017-12-10 16:02:49 +0800

    123

--- main/solver/IntVar.h
+++ main/solver/IntVar.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 using namespace std;
 namespace cudacp {
 class IntVar {
@@ -10,13 +10,20 @@ public:
 	IntVar() {};
 	IntVar(const int id, const string name, const vector<int> values);
 	bool constains(const int value);
-	int size() const ;
+	int size() const;
+	void swap(const int i, const int j);
+	void bind(const int v);
+	void remove(const int v);
+	void clear_marks();
+	void mark(const int v);
+	void restrict();
 protected:
 	int id_;
-	int cur_size_;
+	int curr_size_;
 	int init_size_;
+	int mark_;
 	string name_;
 	vector<int> dom_;
-	map<int, int> map_;
+	unordered_map<int, int> map_;
 };
 }
