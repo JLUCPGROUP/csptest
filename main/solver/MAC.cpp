003cbfe788da4de94db567796d2ca9a83812e1e2
李哲 <leezear@live.cn>
2017-12-28 12:55:04 +0800

    123

--- main/solver/MAC.cpp
+++ main/solver/MAC.cpp
@@ -21,6 +21,63 @@ MAC::MAC(Network * n, const ACAlgorithm ac_algzm) :
 	}
 }
 
+//SearchStatistics MAC::enforce(const int time_limits) {
+//	Timer t;
+//	consistent_ = ac_->EnforceGAC_arc(n_->vars, 0);
+//	x_evt_.clear();
+//	if (!consistent_) {
+//		statistics_.solve_time = t.elapsed();
+//		return statistics_;
+//	}
+//
+//	while (!finished_) {
+//		if (t.elapsed() > time_limits) {
+//			statistics_.solve_time = t.elapsed();
+//			statistics_.time_out = true;
+//			return statistics_;
+//		}
+//
+//		IntVal v_a = select_v_value();
+//		I.push(v_a);
+//		cout << v_a << endl;
+//		++statistics_.num_positive;
+//		v_a.v()->ReduceTo(v_a.a(), I.size());
+//		x_evt_.push_back(v_a.v());
+//		consistent_ = ac_->EnforceGAC_arc(x_evt_, I.size());
+//		cout << ac_->del() << endl;
+//		x_evt_.clear();
+//
+//		if (consistent_&&I.full()) {
+//			cout << I << endl;
+//			finished_ = true;
+//			//++sol_count_;
+//			//consistent_ = false;
+//		}
+//
+//		while (!consistent_ && !I.empty()) {
+//			v_a = I.pop();
+//
+//			for (IntVar* v : n_->vars)
+//				if (!v->assigned())
+//					v->RestoreUpTo(I.size() + 1);
+//
+//			v_a.v()->RemoveValue(v_a.a(), I.size());
+//			cout << "!=" << v_a << endl;
+//			++statistics_.num_negative;
+//			x_evt_.push_back(v_a.v());
+//			consistent_ = v_a.v()->size() && ac_->EnforceGAC_arc(x_evt_, I.size());
+//			cout << ac_->del() << endl;
+//			x_evt_.clear();
+//		}
+//
+//		if (!consistent_)
+//			finished_ = true;
+//	}
+//
+//	statistics_.solve_time = t.elapsed();
+//	return statistics_;
+//}
+
 SearchStatistics MAC::enforce(const int time_limits) {
 	Timer t;
 	consistent_ = ac_->EnforceGAC_arc(n_->vars, 0);
@@ -39,16 +96,14 @@ SearchStatistics MAC::enforce(const int time_limits) {
 
 		IntVal v_a = select_v_value();
 		I.push(v_a);
-		cout << v_a << endl;
 		++statistics_.num_positive;
 		v_a.v()->ReduceTo(v_a.a(), I.size());
 		x_evt_.push_back(v_a.v());
 		consistent_ = ac_->EnforceGAC_arc(x_evt_, I.size());
-		cout << ac_->del() << endl;
 		x_evt_.clear();
 
 		if (consistent_&&I.full()) {
-			cout << I << endl;
+			//cout << I << endl;
 			finished_ = true;
 			//++sol_count_;
 			//consistent_ = false;
@@ -62,11 +117,9 @@ SearchStatistics MAC::enforce(const int time_limits) {
 					v->RestoreUpTo(I.size() + 1);
 
 			v_a.v()->RemoveValue(v_a.a(), I.size());
-			cout << "!=" << v_a << endl;
 			++statistics_.num_negative;
 			x_evt_.push_back(v_a.v());
 			consistent_ = v_a.v()->size() && ac_->EnforceGAC_arc(x_evt_, I.size());
-			cout << ac_->del() << endl;
 			x_evt_.clear();
 		}
 
--- main/solver/Tabular.h
+++ main/solver/Tabular.h
@@ -1,20 +1,20 @@
-﻿#pragma once
-#include "IntVar.h"
-
-using namespace std;
-
-namespace cudacp {
-class Tabular {
-public:
-	Tabular(const int id, const vector<IntVar*>& scope, vector<vector<int>>& tuples) :
-		id_(id), scope_(scope), tuples_(tuples) {};
-	~Tabular() {};
-
-	vector<int>& GetFirstValidTuple(IntVal& v_a);
-protected:
-	int id_;
-	vector<IntVar*> scope_;
-	vector<vector<int>> tuples_;
-
-};
-}
+﻿//#pragma once
+//#include "IntVar.h"
+//
+//using namespace std;
+//
+//namespace cudacp {
+//class Tabular {
+//public:
+//	Tabular(const int id, const vector<IntVar*>& scope, vector<vector<int>>& tuples) :
+//		id_(id), scope_(scope), tuples_(tuples) {};
+//	~Tabular() {};
+//
+//	vector<int>& GetFirstValidTuple(IntVal& v_a);
+//protected:
+//	int id_;
+//	vector<IntVar*> scope_;
+//	vector<vector<int>> tuples_;
+//
+//};
+//}
