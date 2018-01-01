08131598840732412d8969877cbcc3ad919c8b64
李哲 <leezear@live.cn>
2018-01-02 00:39:12 +0800

    123

---
+++ main/solver/SAC1.cpp
@@ -0,0 +1,34 @@
+﻿#include "SAC1.h"
+
+namespace cudacp {
+SAC1::SAC1(Network* n, const ACAlgorithm a) :
+	n_(n), {
+	switch (a) {
+	case AC_3:
+		ac_ = new AC3(n_);
+		break;
+	case AC_3bit:
+		ac_ = new AC3bit(n_);
+		break;
+	case A_FC:
+		ac_ = new FC(n_);
+		break;
+	case A_FC_bit:
+		ac_ = new FCbit(n_);
+		break;
+		//case AC_3rm:
+		//	ac_ = new AC3rm(nt_);
+	default:
+		break;
+	}
+}
+SAC1::~SAC1() {
+	// TODO Auto-generated destructor stub
+
+}
+
+bool SAC1::enforce() const {
+
+}
+
+}
---
+++ main/solver/SAC1.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Network.h"
+#include "Solver.h"
+
+namespace cudacp {
+class SAC1 {
+public:
+	SAC1(Network* n, ACAlgorithm a);
+	bool enforce() const;
+	virtual ~SAC1();
+protected:
+	Network* n_;
+	AC* ac_;
+	ACAlgorithm ac_algzm_;
+};
+}
+
